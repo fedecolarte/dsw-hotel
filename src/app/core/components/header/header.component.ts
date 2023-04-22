58f839ac68d724b97ef004ef046967f524cecd16
fcolarte <fcolarte@lasegunda.com.ar>
2023-04-22 01:47:17 -0300

    header design in desktop

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  hotelLogo: string = '../../../../assets/images/temporal-logo.png'
   constructor() { }
 
   ngOnInit(): void {
