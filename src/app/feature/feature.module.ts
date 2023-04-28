13a95867add9b9bf2c760aa3e9cd295b6317077a
fcolarte <fcolarte@lasegunda.com.ar>
2023-04-28 00:37:39 -0300

    add routing for all components

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -2,9 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FeatureRoutingModule } from '@app-feature/feature-routing.module';
 import { SharedModule } from '@app-shared/shared.module';
+import { ReservationComponent } from './reservation/reservation.component';
+import { RoomTypesComponent } from './room-types/room-types.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
+    ReservationComponent,
+    RoomTypesComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
