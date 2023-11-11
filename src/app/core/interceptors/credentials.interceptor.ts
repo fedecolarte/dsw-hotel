dc657e33f67d8bc123d69c24c1338365ebd36025
fcolarte <fcolarte@lasegunda.com.ar>
2023-11-11 19:13:24 -0300

    add credentials.interceptor for add token in requests

---
+++ src/app/core/interceptors/credentials.interceptor.ts
@@ -0,0 +1,27 @@
+import { HttpEvent, HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, from } from 'rxjs';
+import { concatMap, mergeMap, take } from 'rxjs/operators';
+import { StoreService } from '../services/store.service';
+
+@Injectable()
+export class CredentialsInterceptor implements HttpInterceptor {
+
+  authReq: HttpRequest<any>;
+
+  constructor(private storeService: StoreService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.authReq = request;
+    return this.storeService.token$.pipe(take(1), concatMap((token) => {
+        console.log(token)
+        this.authReq = request.clone({
+            headers: new HttpHeaders({
+            Authorization: `Bearer ${token}`
+            })
+        });
+        return next.handle(this.authReq);
+    }));
+  }
+
+}
